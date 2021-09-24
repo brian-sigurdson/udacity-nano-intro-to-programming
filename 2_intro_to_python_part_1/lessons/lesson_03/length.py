93c0bbd2e5d74a5ebf4b562f7ace399d36a2bab8
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-09-24 15:51:25 -0400

    an exercise

---
+++ 2_intro_to_python_part_1/lessons/lesson_03/length.py
@@ -0,0 +1,23 @@
+# Add your code here.
+def total_length(string_lists):
+    count = 0
+    for a_string in string_lists:
+        if a_string is not None:
+            count += len(a_string)
+
+    return count
+
+
+# Should return 6:
+print(total_length(['foo', 'bar']))
+
+# Should return 0 (it's an empty list):
+print(total_length([]))
+
+# Should return 8:
+print(total_length(['balloons']))
+
+# Should return 0 (it has four empty strings):
+print(total_length(["", '', "", '']))
+
+print(total_length([]))
