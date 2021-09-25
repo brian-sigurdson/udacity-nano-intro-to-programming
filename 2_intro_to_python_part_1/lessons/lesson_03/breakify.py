d06ec73ab556c2d086c2b06cc7a4f5a00ccc3770
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-09-25 13:58:44 -0400

    lesson 3

---
+++ 2_intro_to_python_part_1/lessons/lesson_03/breakify.py
@@ -0,0 +1,10 @@
+lines = ["Haiku frogs in snow",
+         "A limerick came from Nantucket",
+         "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
+
+
+def breakify(lines):
+    return "<br>".join(lines)
+
+
+print(breakify(lines))
---
+++ 2_intro_to_python_part_1/lessons/lesson_03/replace.py
@@ -0,0 +1,21 @@
+# Add your code here:
+def replace_substring(string, target, replacement):
+    output = []
+    len_target = len(target)
+    index = 0
+
+    while index < len(string):
+        if string[index:index + len_target] == target:
+            # target found, replace it
+            output.append(replacement)
+            index += len_target
+        else:
+            output.append(string[index])
+            index += 1
+
+    return "".join(output)
+
+
+# Here are some examples you can test it with:
+print(replace_substring('Hot SPAM!drop soup, and curry with SPAM!plant.', 'SPAM!', 'egg'))
+print(replace_substring("The word 'definately' is definately often misspelled.", 'definately', 'definitely'))
