92047b8e6219ce49d56d1c2a1b9c6d662690a60a
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-09-26 18:00:01 -0400

    exercise

--- 4_intro_to_python_part_2/lessons/lesson_04/order_breakfast.py
+++ 4_intro_to_python_part_2/lessons/lesson_04/order_breakfast.py
@@ -1,34 +1,61 @@
-# vars
-stop = False
+import time
 
 
-# methods
+def print_pause(string):
+    print(string)
+    time.sleep(0)
+
+
+def intro():
+    print_pause("Hello! I am Bob, the Breakfast Bot.")
+    print_pause("Today we have two breakfasts available.")
+    print_pause("The first is waffles with strawberries and whipped cream.")
+    print_pause("The second is sweet potato pancakes with butter and syrup.")
+
+
+def validate_input(prompt, options):
+    while True:
+        input_response = input(prompt).lower()
+
+        for option in options:
+            if option in input_response:
+                return option
+
+        print_pause("Sorry, I don't understand.")
+
+
 def order_again():
     while True:
-        another_order = input("Do you want another order? (yes or no)\n")
-        if "yes" in another_order.lower() or "y" in another_order.lower():
-            return True
-        elif "no" in another_order.lower() or "n" in another_order.lower():
-            return False
+        another_order = validate_input("Do you want another order? (yes or no)\n", ["yes", "y", "no", "n"]).lower()
+        if "yes" in another_order or "y" in another_order:
+            get_order()
+        elif "no" in another_order or "n" in another_order:
+            return
         else:
-            print("Sorry.  I didn't understand that response.")
+            print_pause("Sorry.  I didn't understand that response.")
 
 
-while not stop:
-    response = input("Please place your order.  Would you like waffles or pancakes?\n")
+def get_order():
+    response = validate_input(
+        """Please place your order. Would you like waffles or pancakes?\n""",
+        ["waffles", "pancakes"]
+    ).lower()
 
-    if "waffles" in response.lower():
-        print("Waffles it is!")
-        if order_again():
-            continue
-        else:
-            break
-
-    elif "pancakes" in response.lower():
-        print("Pancakes it is!")
-        if order_again():
-            continue
-        else:
-            break
+    if "waffles" in response:
+        print_pause("Waffles it is!")
+    elif "pancakes" in response:
+        print_pause("Pancakes it is!")
     else:
-        print("Sorry.  I don't understand that response.")
+        print_pause("Sorry.  I don't understand that response.")
+
+    print_pause("Your order will be ready shortly.")
+
+
+def order_breakfast():
+    intro()
+    get_order()
+    order_again()
+    print_pause("Okay.  Good-bye.")
+
+
+order_breakfast()
