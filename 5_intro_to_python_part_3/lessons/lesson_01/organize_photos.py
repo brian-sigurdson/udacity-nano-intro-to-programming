9f6885582912e87cfd62c5e7416ed62ca46738cd
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-09-28 16:28:08 -0400

    updates

--- 5_intro_to_python_part_3/lessons/lesson_01/organize_photos.py
+++ 5_intro_to_python_part_3/lessons/lesson_01/organize_photos.py
@@ -1,32 +1,11 @@
 import os
 
 
-def locate_delimiters(string, target):
-    found_at = []
-
-    if target in string:
-        num_targets = string.count(target)
-        if num_targets != 2:
-            return -1
-        else:
-            first_target_index = string.find(target)
-            string = string[first_target_index + 1:]
-            second_target_index = string.find(target)
-            # print(string)
-            # print(second_target_index)
-            found_at.append(first_target_index)
-            found_at.append(first_target_index + 1 + second_target_index)
-            return found_at
-    else:
-        return -1
-
-
 def extract_place(string, delimiter):
-    delimiter_positions = locate_delimiters(string, delimiter)
-    # print(string)
-    # print(delimiter_positions)
-    # move the starting position + 1 to skip the first delimiter
-    return string[delimiter_positions[0] + 1: delimiter_positions[1]]
+    # the filename will look something like "2021-03-04_<place>_other-stuff"
+    # This splits the string and returns the second element, which will
+    # contain the place call "<place>"
+    return string.split(delimiter)[1]
 
 
 photo_files = os.listdir("Photos")
