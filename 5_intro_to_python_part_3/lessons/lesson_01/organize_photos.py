7a3a78da9cc407f6afa1eb599b9aea1e41bfcc2d
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-10-03 19:43:35 -0400

    updates

--- 5_intro_to_python_part_3/lessons/lesson_01/organize_photos.py
+++ 5_intro_to_python_part_3/lessons/lesson_01/organize_photos.py
@@ -1,4 +1,16 @@
 import os
+import shutil
+
+
+originals = os.listdir("Photos")
+places = []
+
+
+def make_place_directories(the_places):
+    # places_set = set(the_places)
+
+    for val in the_places:
+        os.mkdir(f"Photos/{val}")
 
 
 def extract_place(string, delimiter):
@@ -8,11 +20,27 @@ def extract_place(string, delimiter):
     return string.split(delimiter)[1]
 
 
-photo_files = os.listdir("Photos")
-places = []
+def clean_up(the_places):
+    places_set = set(the_places)
 
-for file_name in photo_files:
-    place = extract_place(file_name, "_")
-    places.append(place)
+    for val in places_set:
+        shutil.rmtree(f"Photos/{val}")
 
+
+def extract_create_move():
+    for file_name in originals:
+        place = extract_place(file_name, "_")
+        if place not in places:
+            places.append(place)
+
+
+extract_create_move()
+make_place_directories(places)
 print(places)
+print(os.listdir("Photos"))
+
+
+input("Hit any key to cleanup.\n")
+clean_up(places)
+
+
