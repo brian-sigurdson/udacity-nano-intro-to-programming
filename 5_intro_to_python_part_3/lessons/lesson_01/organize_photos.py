04f17a5bc36da69e0d569595eff7b0987ec22c11
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-10-04 11:51:39 -0400

    updates

--- 5_intro_to_python_part_3/lessons/lesson_01/organize_photos.py
+++ 5_intro_to_python_part_3/lessons/lesson_01/organize_photos.py
@@ -24,8 +24,18 @@ def extract_place(string, delimiter):
 def clean_up(the_places):
     places_set = set(the_places)
 
-    for val in places_set:
-        shutil.rmtree(val)
+    # for each sub-directory created to hold files for a particular place
+    for place in places_set:
+        # get the filenames in the folder
+        filenames = os.listdir(place)
+        # for each filename
+        for filename in filenames:
+            # move it to the parent directory
+            os.rename(filename, os.path.join("../", filename))
+
+        # now remove the sub-directories
+        shutil.rmtree(place)
+
 
 
 def extract_filenames():
