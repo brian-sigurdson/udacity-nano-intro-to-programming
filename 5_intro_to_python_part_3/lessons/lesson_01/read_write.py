b1faeb419f70846bc6c99da72581a741fa321d01
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-10-05 19:21:15 -0400

    updates

--- 5_intro_to_python_part_3/lessons/lesson_01/read_write.py
+++ 5_intro_to_python_part_3/lessons/lesson_01/read_write.py
@@ -3,6 +3,7 @@ with open("output.txt", "w") as output:
         if val % 2 == 0:
             output.write(f"{val}\n")
 
+
 with open("read.txt") as reader:
     with open("copy.txt", "w") as copy:
         copy.write(reader.read())
