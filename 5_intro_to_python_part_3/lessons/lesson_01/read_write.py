e48a61a83c1cfc062d227499665f462ab2255847
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-10-05 17:14:32 -0400

    exercise

---
+++ 5_intro_to_python_part_3/lessons/lesson_01/read_write.py
@@ -0,0 +1,9 @@
+with open("output.txt", "w") as output:
+    for val in range(31):
+        if val % 2 == 0:
+            output.write(f"{val}\n")
+
+with open("read.txt") as reader:
+    with open("copy.txt", "w") as copy:
+        copy.write(reader.read())
+
