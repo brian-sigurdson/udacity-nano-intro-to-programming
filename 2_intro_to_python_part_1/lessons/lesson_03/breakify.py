96bce4fda41685bdc633847b06a5421b4f5ef43d
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-10-05 20:03:21 -0400

    i think I deleted these by accident

---
+++ 2_intro_to_python_part_1/lessons/lesson_03/breakify.py
@@ -0,0 +1,11 @@
+lines = ["Haiku frogs in snow",
+         "A limerick came from Nantucket",
+         "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
+
+
+def breakify(lines):
+    return "<br>".join(lines)
+
+
+print(breakify(lines))
+
---
+++ 2_intro_to_python_part_1/lessons/lesson_03/silly.py
@@ -0,0 +1,36 @@
+import random
+import words
+
+
+def silly_string(nouns, verbs, templates):
+    # Choose a random template.
+    template = random.choice(templates)
+
+    # We'll append strings into this list for output.
+    output = []
+
+    # Keep track of where in the template string we are.
+    index = 0
+
+    # Add a while loop here.
+    while index < len(template):
+        if template[index: index + 8] == "{{noun}}":
+            # noun placeholder
+            output.append(random.choice(nouns))
+            index += 8
+        if template[index: index + 8] == "{{verb}}":
+            # verb placeholder
+            output.append(random.choice(verbs))
+            index += 8
+        else:
+            # no placeholder present
+            output.append(template[index])
+            index += 1
+
+    # After the loop has finished, join the output and return it.
+    return "".join(output)
+
+
+if __name__ == '__main__':
+    print(silly_string(words.nouns, words.verbs, words.templates))
+
