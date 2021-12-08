d2f3283ed842778bcbb3f2f61e1e634add14fbfa
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-12-08 13:27:55 -0500

    working on project

--- 6_intro_to_javascript/project/designs.js
+++ 6_intro_to_javascript/project/designs.js
@@ -1,10 +1,51 @@
-// Select color input
-// Select size input
+// Variables
+var colorPicker = document.getElementById('colorPicker');
+var pixelCanvas = document.getElementById('pixelCanvas');
+var sizePicker = document.getElementById('sizePicker');
+var tableHeight = document.getElementById('inputHeight');
+var tableWidth = document.getElementById('inputWidth');
 
-// When size is submitted by the user, call makeGrid()
+// functions
+function setCellColor(event) {
+    console.log(event);
+    event.target.bgColor = colorPicker.value;
+    // console.log(event.color.bgColor);
+    console.log(colorPicker.value);
+}
 
-function makeGrid() {
+function makeGrid(event) {
+    // turn off default event behavior
+    event.preventDefault();
+    // remove any prior contents of the table
+    pixelCanvas.innerHTML = "";
+    // get the desired table height
+    tableHeight = document.getElementById('inputHeight');
+    // get the desired table width
+    tableWidth = document.getElementById('inputWidth');
 
-// Your code goes here!
+    console.log("height = " + tableHeight.value);
+    console.log("width = " + tableWidth.value);
+    // console.log("pixelCanvas = " + pixelCanvas);
+
+    // create the new table contents
+    for (var row = 0; row < tableHeight.value; row++) {
+        // create a new row
+        var newRow = pixelCanvas.insertRow(row);
+
+        // add cells to the new row
+        for (var cell = 0; cell < tableWidth.value; cell++) {
+            var newCell = newRow.insertCell(cell);
+            // var hello = document.createTextNode("hello");
+            // newCell.appendChild(hello);
+            // add an event listener here for the cell
+            newCell.addEventListener('click', setCellColor);
+        }
+    }
 
 }
+
+// listeners
+// document.getElementById('sizePicker').addEventListener('click', makeGrid);
+document.getElementById('sizePicker').addEventListener('submit', makeGrid);
+// window.addEventListener('load', makeGrid);
+// document.addEventListener('load', makeGrid);
